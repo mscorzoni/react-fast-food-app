950dba8eeda6339a0c7c34b03e645db8d10466f3
Marcos Scorzoni <mlessa@gmail.com>
2018-10-13 12:20:18 +0100

    add checkout modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -68,6 +68,10 @@ class BurgerBuilder extends Component {
   purchaseHandler = () => {
     this.setState({purchasing: true});
   }
+
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
   
  
   render() {
@@ -81,7 +85,7 @@ class BurgerBuilder extends Component {
 
     return(
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler} >
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
