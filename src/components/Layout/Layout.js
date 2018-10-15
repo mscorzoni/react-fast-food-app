b4946cf8a6011df263736a48fc56402d264245af
Marcos Scorzoni <mlessa@gmail.com>
2018-10-15 16:14:13 +0100

    add backdrop side menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-      <Toolbar />
-      <SideDrawer />
-      <main className={classes.content}>
-        {props.children}
-      </main>
-    </Aux>
-  );
+class Layout extends Component {
 
-export default layout;
+  state = {
+    showSideDrawer: false
+  }
+  
+  sideDrawerCloseHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer};
+      });
+  }
+
+  render() {
+    return (
+        <Aux>
+          <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+          <SideDrawer closed={this.sideDrawerCloseHandler} open={this.state.showSideDrawer}/>
+          <main className={classes.content}>
+            {this.props.children}
+          </main>
+        </Aux>
+      );
+  }
+}
+    
+
+export default Layout;
