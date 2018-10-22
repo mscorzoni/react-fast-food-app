dce531f286e7019a74c75a678e7bbab912501305
Marcos Scorzoni <mlessa@gmail.com>
2018-10-22 20:58:45 +0100

    create modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+
 import Aux from '../../../hoc/Aux/Aux';
-import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component {
-
-
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients)
-      .map(igKey => {
-        return (<li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}</li>);
-      });
-    return (
-      <Aux>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the following ingredients: </p>
-        <ul>
-          {ingredientSummary}
-        </ul>
-        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
-        <p>Continue to Checkout?</p>
-        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
-        <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
-      </Aux>
+const orderSummary = (props) => {
+  let ingredientSummary = Object.keys(props.ingredients)
+    .map(igKey => {
+      return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}:</span> {props.ingredients[igKey]}</li>;
+    });
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p>Continue to Checkout?</p> 
+    </Aux>
     );
-  }
-}
+};
 
-
-export default OrderSummary;
+export default orderSummary;
