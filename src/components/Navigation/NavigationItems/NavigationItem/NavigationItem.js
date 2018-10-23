da63053c79724b7a4dc5ef2307d46bfa23f28f56
Marcos Scorzoni <mlessa@gmail.com>
2018-10-23 13:47:12 +0100

    add navigation buttons to toolbar

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './NavigationItem.css';
+
+const navigationItem = (props) => (
+    <li className={classes.NavigationItem}>
+      <a hrenf={props.link}
+        className={props.active ? classes.active : null}
+        >
+        {props.children} 
+      </a>
+    </li>
+  );
+
+export default navigationItem;
