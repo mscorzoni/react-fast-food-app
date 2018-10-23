65cde41b8dfd4e1efde1d60d795f2ca78b903fc4
Marcos Scorzoni <mlessa@gmail.com>
2018-10-23 16:27:05 +0100

    implement side drawer menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux/Aux';
 import classes from '../../hoc/Layout/Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-  <Aux>
-    <Toolbar />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((previousState) => {
+      return {showSideDrawer: !previousState.showSideDrawer};
+    });
+  }
+
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+
+      )
+  }
+
+}
+
+export default Layout;
