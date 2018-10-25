1225f75c8f4d4f4a7911386ca39d0d01e25bd1a7
Marcos Scorzoni <mlessa@gmail.com>
2018-10-25 18:42:45 +0100

    add route links to navbar

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavigationItem.css';
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
-      <a hrenf={props.link}
-        className={props.active ? classes.active : null}
-        >
-        {props.children} 
-      </a>
+      <NavLink 
+          to={props.link} exact
+          activeClassName={classes.active}
+          >
+          {props.children}
+      </NavLink> 
     </li>
   );
 
