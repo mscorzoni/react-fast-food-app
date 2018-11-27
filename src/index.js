f3f8e4529d90e0a547576ca14fe73a42b54098b6
Marcos Lessa <mlessa@gmail.com>
2018-11-27 18:26:36 +0000

    add action creators

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer from './store/reducer';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
-const store = createStore(reducer);
+const store = createStore( burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
   <Provider store={store}>
@@ -18,9 +18,9 @@ const app = (
   </Provider>
   );
 
+
+
+
 ReactDOM.render( app, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
